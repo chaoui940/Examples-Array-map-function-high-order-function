759ad0da3e712e5d1f87e8f917fa8260d293e943
chaoui940 <daging129@gmail.com>
2021-11-16 21:50:57 +0100

    *

--- script.js
+++ script.js
@@ -10,7 +10,29 @@ let days = [
 // create an array with just 3 letters of previous array
 
 let abv = days.map((days)=> {
-	return days.substring(0 , 2);
+	return days.substring(0 , 3);
 })
 
 console.log(abv)
+
+// 
+let people = [{
+	first_name: "Riche",
+	last_name: "Aboubaker"
+},
+{
+	first_name: 'KADD',
+	last_name: 'Missinsn'
+},
+{
+	first_name: 'laala',
+	last_name: 'Jason'
+},
+]
+
+// create new array with the string full name of each person
+
+let fullNames = people.map((person) => {
+	return person.first_name , person.last_name ;
+});
+console.log(fullNames);
